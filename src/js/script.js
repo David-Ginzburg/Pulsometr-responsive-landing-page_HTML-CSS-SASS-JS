ad7f4059fe7800ff49117508a6d8c76e547eb2f8
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-01-03 01:13:32 +0300

    mail sender

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,21 @@ $(document).ready(function(){
     validateForms('#order form');
 
     jQuery(function($){
-      $('input[name=phone]').mask("+7 (999) 999-99-99");
+      $('input[name=phone]').mask("9 (999) 999-99-99");
+   });
+
+   $('form').submit(function(e) {
+     e.preventDefault();
+     $.ajax({
+       type: "POST",
+       url: "mailer/smart.php",
+       data: $(this).serialize()
+     }).done(function() {
+       $(this).find('input').val('');
+       $('#consultation, #order').fadeOut();
+       $('.overlay, #thanks').fadeIn();
+       $('form').trigger('reset');
+     });
+     return false;
    });
   });
